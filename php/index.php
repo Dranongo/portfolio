a139e9bb0400fbb9adb87c09d1a749c5d1e8fe87
Unknown <maximilien.fonteyn@gmail.com>
2019-01-29 10:50:30 +0100

    Modification Index et intégration js

--- php/index.php
+++ php/index.php
@@ -3,6 +3,11 @@
 	require('../include/navbar.php');
 ?>
 
+	<canvas id="canvas" style="position:absolute; top:0; left:0;"></canvas>
+	<div id="buffer" style="display:none;"></div>
+
+	<script src="../js/index.js"></script>
+
 <?php
 	require('../include/footer.php');
 ?>
